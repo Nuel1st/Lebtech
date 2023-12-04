a924dfae3c8fd0a0c4cfaa62593d7d25c39fc300
Emmanuel1 <Nnamdiemmanuel01@gmail.com>
2023-12-04 12:05:36 +0100

    Update urls.py

--- construction/construction/urls.py
+++ construction/construction/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lebtech.urls'))
+    path('', include('lebtech.urls')),
+    url(r' ^media/(?<path>. *)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r' ^media/(?<path>. *)$', serve,{'document_root': settings.MEDIA_ROOT})
 ]
