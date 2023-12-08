a9c98b60454e1f85aca0ef3b7890e9bd7a0022f4
Emmanuel1 <Nnamdiemmanuel01@gmail.com>
2023-12-08 09:15:49 +0100

    new

--- construction/lebtech/views.py
+++ construction/lebtech/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def home(request):
     return render( request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def detail(request):
+    return render(request, 'detail.html')
+
+def project(request):
+    return render(request, 'project.html')
+
+def team(request):
+    return render(request, 'team.html')
+
